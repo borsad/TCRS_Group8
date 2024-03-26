7e54ac10fa087ed64e060bca18eecf60389d7c4b
asemyousif <aaldwaikat@algomau.ca>
2024-03-26 12:35:37 -0400

    ALl completed UI -  done by Asem AL Dwaikat

---
+++ src/main/java/com/example/tcrs_group8/DashboardUI.java
@@ -0,0 +1,25 @@
+package com.example.tcrs_group8;
+
+import com.example.tcrs_group8.Contollers.SignInController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashboardUI extends Application {
+    SignInController signInController=new SignInController();
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("FAQ!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
