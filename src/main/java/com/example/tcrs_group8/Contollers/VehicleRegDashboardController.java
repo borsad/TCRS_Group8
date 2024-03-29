9396712db24952652de61ba8e0f40d415c59d00d
borsad <priyen4@gmail.com>
2024-03-29 00:50:07 -0400

    Worked on the Vehicle registration Backend implementation

--- src/main/java/com/example/tcrs_group8/Contollers/VehicleRegDashboardController.java
+++ src/main/java/com/example/tcrs_group8/Contollers/VehicleRegDashboardController.java
@@ -1,32 +1,55 @@
 package com.example.tcrs_group8.Contollers;
 
+import com.example.tcrs_group8.Services.DBConnector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class VehicleRegDashboardController {
+    String dlNumber=null;
+    String name=null;
+    String userId;
+    String vinNumber=null;
+    String finalVin=null;
+    String registrationId=null;
     SceneController sceneController=new SceneController();
     @FXML
     private Button helpButton;
-
+    @FXML
+    private Label makeResult;
     @FXML
     private Button logoutButton;
 
     @FXML
     private TextField searchField;
-
+    @FXML
+    private Label lblErrors;
+    @FXML
+    private Label makeNumber;
+    @FXML
+    private Label model;
     @FXML
     private Button searchButton;
-
     @FXML
-    private TextField nameField;
-
+    private Label modelResult;
     @FXML
-    private TextField licenseField;
+    private Label licenseField;
+    @FXML
+    private Label nameField;
+    @FXML
+    private Label lblErrorsVin;
+
+
 
     @FXML
     private TextField addressField;
@@ -47,35 +70,7 @@ public class VehicleRegDashboardController {
     @FXML
     private Button submitButton; // Assuming this is the "Submit" button for the form
 
-    // Method to handle Help button action
-    @FXML
-    void onHelpButtonAction() {
-        System.out.println("Help Button Clicked");
-    }
 
-    // Method to handle Logout button action
-    @FXML
-    void onLogoutButtonAction() {
-        System.out.println("Logout Button Clicked");
-    }
-
-    // Method to handle the search for "Search by License #" action
-    @FXML
-    void onSearchAction() {
-        System.out.println("Search Action Performed");
-    }
-
-    // Method to handle Case Details Search button action
-    @FXML
-    void onCaseDetailsSearchAction() {
-        System.out.println("Case Details Search Button Clicked");
-    }
-
-    // Method to handle the submission of the vehicle registration form
-    @FXML
-    void onSubmitAction() {
-        System.out.println("Submit Button Clicked");
-    }
 
     // Method called to initialize the controller after its root element has been completely processed
     @FXML
@@ -100,5 +95,122 @@ public class VehicleRegDashboardController {
         System.out.println("clicked on back button");
     }
 
+    public void clickSearchDriverLicense(ActionEvent actionEvent) throws SQLException {
+        String dlNumber = searchField.getText();
+        if (dlNumber.isEmpty()){
+            setLblError(Color.TOMATO, "Empty License Field");
+        } else if (dlNumber.length()!=16) {
+            setLblError(Color.TOMATO, "Please Enter 16 Digits");
+        } else{
+            String sql = "SELECT * FROM UserDetails Where DriverLicenseNumber = ?";
+            try {
+            PreparedStatement statement= DBConnector.getConnection().prepareStatement(sql);
+            statement.setString(1,dlNumber);
+            ResultSet rSet=statement.executeQuery();
+        if (!rSet.next()) {
+            setLblError(Color.TOMATO, "Can't find the record");
+        }else{ name= rSet.getString(3);
+            nameField.setText(name);
+            licenseField.setText(dlNumber);
+            userId=rSet.getString(1);
+            System.out.println(userId+"UserIDddddddd");
+        }}catch (SQLException ex){
+                System.err.println(ex.getMessage());
+            }
+        }
+
+    }
+    @FXML
+    private void setLblError(Color color, String text) {
+        lblErrors.setTextFill(color);
+        lblErrors.setText(text);
+        System.out.println(text);
+    }
+    @FXML
+    private void setLblErrorVIN(Color color, String text) {
+        lblErrors.setTextFill(color);
+        lblErrors.setText(text);
+        System.out.println(text);
+    }
+    public void vinSearchClick(ActionEvent actionEvent) {
+        vinNumber = vinNumberField.getText();
+        if (vinNumber.isEmpty()){
+            setLblErrorVIN(Color.TOMATO, "Empty Vin Field");
+        } else if (vinNumber.length()!=17) {
+            setLblErrorVIN(Color.TOMATO, "Please Enter 17 Digits");
+        } else{
+            String sql = "SELECT * FROM Vehicle Where VIN = ?";
+            try {
+                PreparedStatement statement= DBConnector.getConnection().prepareStatement(sql);
+                statement.setString(1,vinNumber);
+                ResultSet rSet=statement.executeQuery();
+                if (!rSet.next()) {
+                    setLblErrorVIN(Color.TOMATO, "Can't find the record");
+                }else{
+                    makeResult.setText(rSet.getString(3));
+                    modelResult.setText(rSet.getString(4));
+                    finalVin=rSet.getString(2);
+                    System.out.println(finalVin+"regIDDDDD");
+                }}catch (SQLException ex){
+                System.err.println(ex.getMessage());
+            }
+        }
+
+    }
+
+    public void clickSubmit(ActionEvent actionEvent) throws SQLException {
+        if(makeResult.getText().isEmpty()){
+            Alert helpAlert = new Alert(Alert.AlertType.ERROR);
+            helpAlert.setHeaderText("Error");
+            helpAlert.setContentText("Please Enter VIN First");
+            helpAlert.showAndWait();
+
+        } else if (nameField.getText().isEmpty()){
+            Alert helpAlert = new Alert(Alert.AlertType.ERROR);
+            helpAlert.setHeaderText("Error");
+            helpAlert.setContentText("Please Enter Driver License Details First");
+            helpAlert.showAndWait();
+        } else if(userId == null||finalVin==null){
+            Alert helpAlert = new Alert(Alert.AlertType.ERROR);
+            helpAlert.setHeaderText("Error");
+            helpAlert.setContentText("Please Refill information and try again");
+            helpAlert.showAndWait();
+          //  setLblErrorVIN(Color.TOMATO, "Please Refill information and try again");
+        }else{
+            try {
+                String registrationCheckerQuery = "SELECT * FROM Registration Where VIN = ? AND UserID= ?";
+                String insertSql = "INSERT INTO Registration (VIN, UserID) VALUES (?, ?)";
+                PreparedStatement checkRegistrationExistsStatement = DBConnector.getConnection().prepareStatement(registrationCheckerQuery);
+                checkRegistrationExistsStatement.setString(1, finalVin);
+                checkRegistrationExistsStatement.setString(2, userId);
+                ResultSet checkRegistrationExists = checkRegistrationExistsStatement.executeQuery();
+                if (!checkRegistrationExists.next()) {
+                    PreparedStatement statement = DBConnector.getConnection().prepareStatement(insertSql);
+                    statement.setString(1, finalVin);
+                    statement.setString(2, userId);
+                    int rowsAffected = statement.executeUpdate();
+                    if (rowsAffected > 0) {
+                        System.out.println("Record inserted successfully into LoginCredentials.");
+                    }
+                    else {
+                        Alert helpAlert = new Alert(Alert.AlertType.ERROR);
+                        helpAlert.setHeaderText("Error");
+                        helpAlert.setContentText("The Vehicle is already registered to the same user");
+                        helpAlert.showAndWait();                    }
+
+                }else{
+                    Alert helpAlert = new Alert(Alert.AlertType.ERROR);
+                    helpAlert.setHeaderText("Error");
+                    helpAlert.setContentText("The Vehicle is already registered to the same user");
+                    helpAlert.showAndWait();
+                }
+
+            }
+            catch (SQLException ex) {
+           System.err.println(ex.getMessage());}
+
+        }
+
+    }
 }
 
